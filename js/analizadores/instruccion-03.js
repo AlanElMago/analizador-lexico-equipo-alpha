b66c87c3d6140eaa29875c43be78c2e0b0712ffc
Alan Franco <alanfrancob97@gmail.com>
2024-04-27 20:58:04 -0600

    Refactorizar analizadores de instrucciones Se renombraron algunas variables para que fueran más descriptivas.

--- js/analizadores/instruccion-03.js
+++ js/analizadores/instruccion-03.js
@@ -3,7 +3,7 @@ import { obtenerTipoSimbolo } from "../expresiones-regulares.js";
 import { Tipo } from "../expresiones-regulares.js";
 
 export const instruccion03 = (tokens) => {
-  const sintaxisValida = [
+  const simbolosEsperados = [
     ["si", "osi", "sino"],       // Token 0
     [":", Tipo.Id, Tipo.Numero], // Token 1
     [":", Tipo.Comparacion],     // Token 2
@@ -12,58 +12,58 @@ export const instruccion03 = (tokens) => {
   ];
 
   // Token 0
-  let lineasTexto = [`${tokens[0]}: nombre de la instrucción`];
+  let textoSalida = [`${tokens[0]}: nombre de la instrucción`];
 
   // Token 1
-  if (!sintaxisValida[1].includes(obtenerTipoSimbolo(tokens[1]))) {
-    lineasTexto.push(generarMensajeErrorSintaxis(tokens[1], sintaxisValida[1]));
+  if (!simbolosEsperados[1].includes(obtenerTipoSimbolo(tokens[1]))) {
+    textoSalida.push(generarMensajeErrorSintaxis(tokens[1], simbolosEsperados[1]));
 
-    return lineasTexto;
+    return textoSalida;
   }
 
   if (tokens[1] === ":") {
-    lineasTexto.push(tokens[0] === "sino"
+    textoSalida.push(tokens[0] === "sino"
       ? `${tokens[1]}: fin de la instrucción`
       : generarMensajeErrorSintaxis(tokens[1], [":"]));
 
-    return lineasTexto;
+    return textoSalida;
   }
 
-  lineasTexto.push(`${tokens[1]}: el símbolo para evaluar (${obtenerTipoSimbolo(tokens[1])})`);
+  textoSalida.push(`${tokens[1]}: el símbolo para evaluar (${obtenerTipoSimbolo(tokens[1])})`);
 
   // Token 2
-  if (!sintaxisValida[2].includes(obtenerTipoSimbolo(tokens[2]))) {
-    lineasTexto.push(generarMensajeErrorSintaxis(tokens[2], sintaxisValida[2]));
+  if (!simbolosEsperados[2].includes(obtenerTipoSimbolo(tokens[2]))) {
+    textoSalida.push(generarMensajeErrorSintaxis(tokens[2], simbolosEsperados[2]));
 
-    return lineasTexto;
+    return textoSalida;
   }
 
   if (obtenerTipoSimbolo(tokens[2]) === ":") {
-    lineasTexto.push(`${tokens[2]}: fin de la instrucción`);
+    textoSalida.push(`${tokens[2]}: fin de la instrucción`);
 
-    return lineasTexto;
+    return textoSalida;
   }
 
-  lineasTexto[1] = `${tokens[1]}: el primer símbolo para comparar (${obtenerTipoSimbolo(tokens[1])})`;
-  lineasTexto.push(`${tokens[2]}: operación de comparación`);
+  textoSalida[1] = `${tokens[1]}: el primer símbolo para comparar (${obtenerTipoSimbolo(tokens[1])})`;
+  textoSalida.push(`${tokens[2]}: operación de comparación`);
 
   // Token 3
-  if (!sintaxisValida[3].includes(obtenerTipoSimbolo(tokens[3]))) {
-    lineasTexto.push(generarMensajeErrorSintaxis(tokens[3], sintaxisValida[3]));
+  if (!simbolosEsperados[3].includes(obtenerTipoSimbolo(tokens[3]))) {
+    textoSalida.push(generarMensajeErrorSintaxis(tokens[3], simbolosEsperados[3]));
 
-    return lineasTexto;
+    return textoSalida;
   }
 
-  lineasTexto.push(`${tokens[3]}: el segundo símbolo para comparar (${obtenerTipoSimbolo(tokens[3])})`);
+  textoSalida.push(`${tokens[3]}: el segundo símbolo para comparar (${obtenerTipoSimbolo(tokens[3])})`);
 
   // Token 4
-  if (!sintaxisValida[4].includes(obtenerTipoSimbolo(tokens[4]))) {
-    lineasTexto.push(generarMensajeErrorSintaxis(tokens[4], sintaxisValida[4]));
+  if (!simbolosEsperados[4].includes(obtenerTipoSimbolo(tokens[4]))) {
+    textoSalida.push(generarMensajeErrorSintaxis(tokens[4], simbolosEsperados[4]));
 
-    return lineasTexto;
+    return textoSalida;
   }
 
-  lineasTexto.push(`${tokens[4]}: fin de la instrucción`);
+  textoSalida.push(`${tokens[4]}: fin de la instrucción`);
 
-  return lineasTexto;
+  return textoSalida;
 }
