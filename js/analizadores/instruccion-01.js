a68ee21bbd5165e09c36411492d9b239bc4cc94c
Alan Franco <alanfrancob97@gmail.com>
2024-04-27 20:58:04 -0600

    Refactorización de los analizadores de instrucciones Se renombraron algunas variables para que fueran más descriptivas.

--- js/analizadores/instruccion-01.js
+++ js/analizadores/instruccion-01.js
@@ -3,7 +3,7 @@ import { obtenerTipoSimbolo } from "../expresiones-regulares.js";
 import { Tipo } from "../expresiones-regulares.js";
 
 export const instruccion01 = (tokens) => {
-  const sintaxisValida = [
+  const simbolosEsperados = [
     ["imprimir"],                              // Token 0
     ["("],                                     // Token 1
     [")", Tipo.Id, Tipo.Numero, Tipo.Literal], // Token 2
@@ -11,40 +11,40 @@ export const instruccion01 = (tokens) => {
   ];
 
   // Token 0
-  let lineasTexto = [`${tokens[0]}: nombre de la instrucción`];
+  let textoSalida = [`${tokens[0]}: nombre de la instrucción`];
 
   // Token 1
-  if (!sintaxisValida[1].includes(obtenerTipoSimbolo(tokens[1]))) {
-    lineasTexto.push(generarMensajeErrorSintaxis(tokens[1], sintaxisValida[1]));
+  if (!simbolosEsperados[1].includes(obtenerTipoSimbolo(tokens[1]))) {
+    textoSalida.push(generarMensajeErrorSintaxis(tokens[1], simbolosEsperados[1]));
 
-    return lineasTexto;
+    return textoSalida;
   }
 
-  lineasTexto.push(`${tokens[1]}: paréntesis de apertura de la instrucción`);
+  textoSalida.push(`${tokens[1]}: paréntesis de apertura de la instrucción`);
 
   // Token 2
-    if (!sintaxisValida[2].includes(obtenerTipoSimbolo(tokens[2]))) {
-    lineasTexto.push(generarMensajeErrorSintaxis(tokens[2], sintaxisValida[2]));
+    if (!simbolosEsperados[2].includes(obtenerTipoSimbolo(tokens[2]))) {
+    textoSalida.push(generarMensajeErrorSintaxis(tokens[2], simbolosEsperados[2]));
 
-    return lineasTexto;
+    return textoSalida;
   }
 
   if (obtenerTipoSimbolo(tokens[2]) === ")") {
-    lineasTexto.push(`${tokens[2]}: paréntesis de cierre de la instrucción`);
+    textoSalida.push(`${tokens[2]}: paréntesis de cierre de la instrucción`);
 
-    return lineasTexto;
+    return textoSalida;
   }
 
-  lineasTexto.push(`${tokens[2]}: primer argumento de la instrucción (${obtenerTipoSimbolo(tokens[2])}). Mensaje que se imprimirá al usuario.`);
+  textoSalida.push(`${tokens[2]}: primer argumento de la instrucción (${obtenerTipoSimbolo(tokens[2])}). Mensaje que se imprimirá al usuario.`);
 
   // Token 3
-  if (!sintaxisValida[3].includes(obtenerTipoSimbolo(tokens[3]))) {
-    lineasTexto.push(generarMensajeErrorSintaxis(tokens[3], sintaxisValida[3]));
+  if (!simbolosEsperados[3].includes(obtenerTipoSimbolo(tokens[3]))) {
+    textoSalida.push(generarMensajeErrorSintaxis(tokens[3], simbolosEsperados[3]));
 
-    return lineasTexto;
+    return textoSalida;
   }
 
-  lineasTexto.push(`${tokens[3]}: paréntesis de cierre de la instrucción`);
+  textoSalida.push(`${tokens[3]}: paréntesis de cierre de la instrucción`);
 
-  return lineasTexto;
+  return textoSalida;
 }
--- js/analizadores/instruccion-04.js
+++ js/analizadores/instruccion-04.js
@@ -3,7 +3,7 @@ import { obtenerTipoSimbolo } from "../expresiones-regulares.js";
 import { Tipo } from "../expresiones-regulares.js";
 
 export const instruccion04 = (tokens) => {
-  const sintaxisValida = [
+  const simbolosEsperados = [
     ["para"],                // Token 0
     [Tipo.Id],               // Token 1
     ["en"],                  // Token 2
@@ -12,43 +12,43 @@ export const instruccion04 = (tokens) => {
   ];
 
   // Token 0
-  let lineasTexto = [`${tokens[0]}: nombre de la instrucción`];
+  let textoSalida = [`${tokens[0]}: nombre de la instrucción`];
 
   // Token 1
-  if (!sintaxisValida[1].includes(obtenerTipoSimbolo(tokens[1]))) {
-    lineasTexto.push(generarMensajeErrorSintaxis(tokens[1], sintaxisValida[1]));
+  if (!simbolosEsperados[1].includes(obtenerTipoSimbolo(tokens[1]))) {
+    textoSalida.push(generarMensajeErrorSintaxis(tokens[1], simbolosEsperados[1]));
 
-    return lineasTexto;
+    return textoSalida;
   }
 
-  lineasTexto.push(`${tokens[1]}: variable de control del ciclo`);
+  textoSalida.push(`${tokens[1]}: variable de control del ciclo`);
 
   // Token 2
-  if (!sintaxisValida[2].includes(tokens[2])) {
-    lineasTexto.push(generarMensajeErrorSintaxis(tokens[2], sintaxisValida[2]));
+  if (!simbolosEsperados[2].includes(tokens[2])) {
+    textoSalida.push(generarMensajeErrorSintaxis(tokens[2], simbolosEsperados[2]));
 
-    return lineasTexto;
+    return textoSalida;
   }
 
-  lineasTexto.push(`${tokens[2]}: palabra reservada para indicar el inicio del rango`);
+  textoSalida.push(`${tokens[2]}: palabra reservada para indicar el inicio del rango`);
 
   // Token 3
-  if (!sintaxisValida[3].includes(obtenerTipoSimbolo(tokens[3]))) {
-    lineasTexto.push(generarMensajeErrorSintaxis(tokens[3], sintaxisValida[3]));
+  if (!simbolosEsperados[3].includes(obtenerTipoSimbolo(tokens[3]))) {
+    textoSalida.push(generarMensajeErrorSintaxis(tokens[3], simbolosEsperados[3]));
 
-    return lineasTexto;
+    return textoSalida;
   }
 
-  lineasTexto.push(`${tokens[3]}: la secuencia de valores que tomará la variable de control (${obtenerTipoSimbolo(tokens[3])})`);
+  textoSalida.push(`${tokens[3]}: la secuencia de valores que tomará la variable de control (${obtenerTipoSimbolo(tokens[3])})`);
 
   // Token 4
-  if (!sintaxisValida[4].includes(obtenerTipoSimbolo(tokens[4]))) {
-    lineasTexto.push(generarMensajeErrorSintaxis(tokens[4], sintaxisValida[4]));
+  if (!simbolosEsperados[4].includes(obtenerTipoSimbolo(tokens[4]))) {
+    textoSalida.push(generarMensajeErrorSintaxis(tokens[4], simbolosEsperados[4]));
 
-    return lineasTexto;
+    return textoSalida;
   }
 
-  lineasTexto.push(`${tokens[4]}: fin de la instrucción`);
+  textoSalida.push(`${tokens[4]}: fin de la instrucción`);
 
-  return lineasTexto;
+  return textoSalida;
 }
