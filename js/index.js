96294ebbe0d316c75e70aa477ef8bf9d9fb1774e
Alan Franco <alanfrancob97@gmail.com>
2024-09-27 20:37:18 -0600

    Modificar index.html e index.js
    Refactorización de los archivos index.html e index.js

--- js/index.js
+++ js/index.js
@@ -1,98 +1,101 @@
 import { Scanner } from './scanner.js';
-import { Parser } from './parser_deprecado.js';
-import { Paarser } from './parser.js';
+import { Parser } from './parser.js';
 import { generarArbolSintactico } from './arbol_d3.js';
-import { analizarLexico, analizarSintactico } from './a_lex-sintax.js';
 
-const btnAnalizar = document.getElementById('btn-analizar');
-const btnSintax = document.getElementById('btn-sintax');
-const btnLexSintax = document.getElementById('btn-lexsintax');
+const btnLexico = document.getElementById('btn-lexico');
+const btnSintactico = document.getElementById('btn-sintactico');
+const btnLexicoSintactico = document.getElementById('btn-lexico-y-sintactico');
 const entrada = document.getElementById('entrada');
 const salida = document.getElementById('salida');
 const btnLimpiar = document.getElementById('btn-limpiar');
 const btnEquipo = document.getElementById('btn-equipo');
 const btnSalir = document.getElementById('btn-salir');
 
-btnAnalizar.addEventListener('click', (event) => {
-    event.preventDefault();
-    document.getElementById("tree-container").innerHTML = '';
-    const texto = entrada.value;
-    const scanner = new Scanner(texto);
-    const parser = new Parser(scanner);
-    const textoSalida = parser.parsear();
-    salida.value = textoSalida.join('\n');
+const analisisLexico = () => {
+  const texto = entrada.value;
+  const scanner = new Scanner(texto);
+  const parser = new Parser(scanner);
+
+  parser.parsear();
+
+  salida.value = parser.analisis.join("\n");
+
+  if (parser.hayErrores()) {
+    salida.value += "\n";
+    salida.value += parser.errores.join("\n");
+  }
+}
+
+const analisisSintactico = () => {
+  const texto = entrada.value;
+  const scanner = new Scanner(texto);
+  const parser = new Parser(scanner);
+  const arbol = parser.parsear();
+
+  generarArbolSintactico(arbol);
+};
+
+
+btnLexico.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  salida.style.display = 'block';
+  document.getElementById("tree-container").innerHTML = "";
+  document.getElementById("tree-container").style.display = 'none';
+
+  analisisLexico();
 });
 
-btnSintax.addEventListener('click', (event) => {
-    event.preventDefault();
-    const texto = entrada.value;
-    const scanner = new Scanner(texto);
-    const paarser = new Paarser(scanner);
-    const arbol = paarser.parsear();
+btnSintactico.addEventListener('click', (event) => {
+  event.preventDefault();
 
-    if (paarser.hayErrores()) {
-        salida.value = "Error al generar el árbol sintáctico";
-        return;
-    }
+  salida.style.display = 'none';
+  document.getElementById("tree-container").innerHTML = "";
+  document.getElementById("tree-container").style.display = 'block';
 
-    document.getElementById("tree-container").innerHTML = '';
-    document.getElementById("tree-container").style.display = 'block';
+  analisisSintactico();
+})
 
-    generarArbolSintactico(arbol);
+btnLexicoSintactico.addEventListener('click', (event) => {
+  event.preventDefault();
 
-    salida.style.display = 'none';
-});
+  salida.style.display = 'block';
+  document.getElementById("tree-container").innerHTML = "";
+  document.getElementById("tree-container").style.display = 'block';
 
-btnLexSintax.addEventListener('click', (event) => {
-    event.preventDefault();
-    salida.style.display = 'block';
-    const texto = entrada.value;
-
-    // Análisis léxico
-    salida.value = analizarLexico(texto);
-
-    // // Análisis sintáctico
-    const resultado = analizarSintactico(texto);
-
-    // Limpiar el contenedor del árbol y mostrarlo
-    document.getElementById("tree-container").innerHTML = '';
-    document.getElementById("tree-container").style.display = 'block';
-
-    if (resultado.error) {
-        salida.value += "\n" + resultado.mensaje;
-    } else {
-        generarArbolSintactico(resultado.arbol);
-    }
+  analisisLexico();
+  analisisSintactico();
 });
 
 btnLimpiar.addEventListener('click', (event) => {
-    event.preventDefault();
-    entrada.value = '';
-    salida.value = '';
-    salida.style.display = 'block';
-    
-    const treeContainer = document.getElementById("tree-container");
-    while (treeContainer.firstChild) {
-        treeContainer.removeChild(treeContainer.firstChild);
-    }
+  event.preventDefault();
+
+  entrada.value = "";
+  salida.value = "";
+  salida.style.display = 'block';
+  
+  const treeContainer = document.getElementById("tree-container");
+  while (treeContainer.firstChild) {
+      treeContainer.removeChild(treeContainer.firstChild);
+  }
 });
 
 btnEquipo.addEventListener('click', (event) => {
-    event.preventDefault();
-    document.getElementById('Modal').style.display = 'block';
+  event.preventDefault();
+  document.getElementById('Modal').style.display = 'block';
 });
 
-document.getElementsByClassName('close')[0].addEventListener('click', function() {
+document.getElementsByClassName('close')[0].addEventListener('click', () => {
+  document.getElementById('Modal').style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+  if (event.target == document.getElementById('Modal')) {
     document.getElementById('Modal').style.display = 'none';
-});
-
-window.addEventListener('click', function(event) {
-    if (event.target == document.getElementById('Modal')) {
-        document.getElementById('Modal').style.display = 'none';
-    }
+  }
 });
 
 btnSalir.addEventListener('click', (event) => {
-    event.preventDefault();
-    window.close();
+  event.preventDefault();
+  window.close();
 });
